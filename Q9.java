4ab9d6b39a4016da0dc57b22bd839be50e890d72
Ayush Majumder <126413422+dynoayush@users.noreply.github.com>
2024-02-22 23:20:49 +0530

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -1,3 +1,6 @@
+// A prime number is a positive integer that is divisible by exactly 2 integers, 1 and the number itself.
+//You are given a number 'n'.
+//Find out whether 'n' is prime or not.
 public class Q9 {
     public static String isPrime(int num) {
         if(num==1)
